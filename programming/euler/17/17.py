9e219be221cc7e60206f6d5d4274d7eb4e1f8ba8
Lali <singh0833@gmail.com>
2013-04-17 20:18:02 -0400

    updated 17 to work

--- programming/euler/17/17.py
+++ programming/euler/17/17.py
@@ -1,8 +1,9 @@
-#length of one, two, three, four... nineteen 
-words_len = [3, 3, 5, 4, 4, 3, 5, 5, 4, 3, 6, 6, 8, 8, 7, 7, 9, 8, 8 ]
+words_to_19 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+words_len = map(lambda x: len(x), words_to_19)
 
-#length of twenty, thirty, fourty... ninety
-words_teens_len = [6, 6, 6, 5, 5, 7, 6, 6]
+words_teens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+#words_teens_len = [6, 6, 6, 5, 5, 7, 6, 6]
+words_teens_len = map(lambda x: len(x), words_teens)
 
 #length of 'hundred'
 h_len = 7
@@ -23,19 +24,19 @@ b = b*10 + sum(words_len[0:9])*8
 
 # sum 100 to 999
 
-# all 'hundred' from 100 to 999 
-c =  h_len * 100 * 9 
-
-# all 1 to 99 for each hundred (100s, 200s, etc) 
-c = c + (a+b)*9
-
 # all ones, twos, threes beginning the 100s like
 # one hundred, one hundred and one, two hundred, two hundred and one
-c = c + sum(words_len[0:9]) * 100 * 9
+c = sum(words_len[0:9]) * 100
+
+# all 'hundred' from 100 to 999 
+c = c + h_len * 100 * 9 
 
 # all 'and' in hundreds such as one hundred 'and' one,
 # one hundred 'and' two, etc
 c = c + 99 * 9 * a_len
+
+# all 1 to 99 for each hundred (100s, 200s, etc) 
+c = c + (a+b)*9
 
 # one thousand
 d = len('one') + len('thousand')
